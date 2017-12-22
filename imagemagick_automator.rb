c3cb3a0b428985545c80de9fcea93913775d6d23
Andrew Mione <mione.a@gmail.com>
2017-12-22 15:34:14 -0500

    completed imagemagick script to resize all image in directory, and skip already resized images

--- imagemagick_automator.rb
+++ imagemagick_automator.rb
@@ -3,18 +3,24 @@ require 'fileutils'
 
 directory = ARGV[0]
 
-directory = "/home/amione/photo_blog_rebuild/uploads"
+@files = Dir["#{directory}*.jpg"]
   
-fix_dir = File.join(directory, "/")
-  
-@files = Dir["#{fix_dir}/*.jpg"]
-  
-puts @files
-
-1008x756
+#puts @files
 
 @files.each do |single_file|
+	
+	filename = single_file.to_s.split('/').last
 	filename_split1 = single_file.to_s.split('.jpg').first
-	filename2 = filename_split1.split('//').last
-	puts filename2
+	filename3 = filename_split1+"_resize.jpg"
+
+#	puts filename
+#	puts filename3
+
+	if filename.include?('resize') == false && File.exist?(filename3) == false
+		puts "converting #{single_file}"
+		puts "convert #{single_file} -resize 1008x756 #{filename3}"
+		system "convert #{single_file} -resize 1008x756 #{filename3}"
+	else
+		puts "#{single_file} already resized"
+	end
 end
